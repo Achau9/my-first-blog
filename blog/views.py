69d017a35ce1f4f2a5d84136b848e099c9c07152
Achau9 <achau@afsenyc.org>
2016-05-31 11:26:29 -0400

    Drafts

--- blog/views.py
+++ blog/views.py
@@ -8,14 +8,21 @@ from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-    
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+    
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
