5bb33cae38a332f0f378993ce673c5bfea5c4878
Achau9 <achau@afsenyc.org>
2016-06-09 23:20:38 -0400

    Buttons

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.db import models
 from django.contrib.auth.decorators import login_required
 
 def button_clicked(request):
-	if request.GET.get('Next') == 'Next':
-			return render(request, 'blog/post_list.html', {'posts': posts})
+	
+	return render(request, 'blog/post_list.html', {'posts': posts})
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
